f1fe4ee97cdca37e2baaa92bfec5a3659029dd23
pasha <pasha.brel@gmail.com>
2018-04-01 02:27:34 +1100

    1.0

---
+++ Assets/DebugHealthUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugHealthUI : MonoBehaviour
+{
+    private PlayerHealth myHealth;
+    private Image myImage;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    myHealth = GetComponentInParent<PlayerHealth>();
+	    myImage = GetComponent<Image>();
+	    myHealth.OnHealthChange += Set;
+	}
+
+    public void Set(float health)
+    {
+       
+        myImage.fillAmount = health / myHealth.maxHealth;
+    }
+
+    
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+
+    public bool healthRegenerates;
+    public float regenerationRate;
+    
+    public Coroutine regenRoutine;
+    
+
+    public event Action<float> OnHealthChange;
+    public event Action OnHealthZero;
+
+    
+
+
+    void Awake()
+    {
+        OnHealthZero += () =>
+        {
+            if (regenRoutine != null)
+            {
+                StopCoroutine(regenRoutine);
+            }
+        };
+
+        if (healthRegenerates)
+        {
+            StartRegenRoutine();
+        }
+
+
+    }
+
+    void Start()
+    {
+        if (OnHealthChange != null) OnHealthChange(health);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeHealth(10);
+        }
+    }
+
+    public void ChangeHealth(float change)
+    {
+        if (health > 0)
+        {
+            health += change;
+          
+        }
+        if(health <= 0)
+        {
+            if (OnHealthZero != null) OnHealthZero();
+        }
+        else if(health>maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        if (OnHealthChange != null) OnHealthChange(health);
+    }
+
+    public void HealEffect(float rate, float healthAdded)
+    {
+        StartCoroutine(HealEffectTimer(rate, healthAdded));
+    }
+
+    public IEnumerator HealEffectTimer(float rate, float healthAdded)
+    {
+        float duration = healthAdded / rate;
+        regenerationRate += rate;
+        while (true)
+        {
+            duration -= Time.deltaTime;
+
+            if (duration <= 0)
+            {
+                break;
+            }
+            //yield return new WaitForEndOfFrame();
+            yield return null;
+        }
+
+        regenerationRate -= rate;
+
+        yield return null;
+    }
+
+    public void StartRegenRoutine()
+    {
+        regenRoutine = StartCoroutine(HealRoutine());
+    }
+
+    public IEnumerator HealRoutine()
+    {
+        float healthPiece = 0;
+        while (true)
+        {
+            healthPiece = Time.deltaTime * regenerationRate;
+            
+            ChangeHealth(healthPiece);
+            
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+}
